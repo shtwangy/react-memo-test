cd68644193cfeee7213f86170475561d299b0db2
shtwangy <shtwangy@gmail.com>
2020-11-18 21:22:36 +0900

    feat(src/App.tsx): create UI that allows to see movement of child components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC, useState} from 'react';
+import {SfcChild, MemoChild, DeepEqualMemoChild} from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface Obj {
+  deepTrigger: number
 }
+
+const App: FC = () => {
+  const [sfcTrigger, setSfcTrigger] = useState<number>(0)
+  const [memoTrigger, setMemoTrigger] = useState<number>(0)
+  const [obj, setObject] = useState<Obj>({deepTrigger: 0})
+
+  const countUpSfc = () => {
+    setSfcTrigger(prevState => prevState + 1)
+  }
+
+  const countUpMemo = () => {
+    setMemoTrigger(prevState => prevState + 1)
+  }
+
+  const countUpDeepTrigger = () => {
+    setObject(prevState => {
+      return {deepTrigger: prevState.deepTrigger + 1}
+    })
+  }
+
+  return (
+      <div>
+        <h2>React Memo Test</h2>
+        <button onClick={countUpSfc}>SFC</button>
+        <button onClick={countUpMemo}>Memo</button>
+        <button onClick={countUpDeepTrigger}>Deep Equal Memo</button>
+
+        <SfcChild trigger={sfcTrigger} />
+        <MemoChild trigger={memoTrigger} />
+        <DeepEqualMemoChild trigger={memoTrigger} obj={obj}/>
+      </div>
+  );
+};
 
 export default App;
